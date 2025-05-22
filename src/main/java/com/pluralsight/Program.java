8a3a72cc2644118e565affe24ebcf0b479d3067a
arthurmov <officialamovsesyan@gmail.com>
2025-05-22 10:19:35 -0400

    Created Chips, Drink, Sandwich and UserInterface classes + Created reciepts folder

---
+++ src/main/java/com/pluralsight/Program.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+
+public class Program {
+    public static void main(String[] args) {
+
+        System.out.println(ColorCodes.RED + """ 
+                  
+ ___  __   __  _____   ___     __  __  __   __    ___     _     _  _   ___   __      __  ___    ___   _  _\s
+| _ ) \\ \\ / / |_   _| | __|   |  \\/  | \\ \\ / /   / __|   /_\\   | \\| | |   \\  \\ \\    / / |_ _|  / __| | || |
+| _ \\  \\ V /    | |   | _|    | |\\/| |  \\ V /    \\__ \\  / _ \\  | .` | | |) |  \\ \\/\\/ /   | |  | (__  | __ |
+|___/   |_|     |_|   |___|   |_|  |_|   |_|     |___/ /_/ \\_\\ |_|\\_| |___/    \\_/\\_/   |___|  \\___| |_||_|
+                                                                                                                 \s """ + ColorCodes.RESET + """
+                                                                      
+Deli-cious Sandwiches  |  Crafted with Java — Capstone Edition
+""");
+
+        UserInterface ui = new UserInterface();
+
+    }
+}
