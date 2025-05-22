c43f650aebd2e477efe3dbdd0c85e3de5b0ba6aa
arthurmov <officialamovsesyan@gmail.com>
2025-05-22 13:48:47 -0400

    Saving changes - Added Price interface and implemented inheritance in toppings

---
+++ src/main/java/com/pluralsight/Price.java
@@ -0,0 +1,5 @@
+package com.pluralsight;
+
+public interface Price {
+    double getValue();
+}
---
+++ src/main/java/com/pluralsight/Topping.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+public class Topping implements Price {
+    private String name;
+
+    @Override
+    public double getValue() {
+        return 0;
+    }
+}
